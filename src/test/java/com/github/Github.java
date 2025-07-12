f9310f198e64e0d131e5241cee243ab9d2e88501
MelvinMoses06 <dmelvinmoses@gmail.com>
2025-07-12 17:33:44 +0530

    New File Added

---
+++ src/test/java/com/github/Github.java
@@ -0,0 +1,20 @@
+package com.github;
+
+public class Github {
+	
+	public void apple() {
+		
+		System.out.println("Apple");
+	}
+	
+	public void banana() {
+		
+		System.out.println("Banana");
+	}
+	
+	public void orange() {
+		
+		System.out.println("Orange");
+	}
+
+}
