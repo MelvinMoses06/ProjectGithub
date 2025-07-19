7c1d56041fcd0dfe8ab470bc2dc9415ba1293e0a
MelvinMoses06 <dmelvinmoses@gmail.com>
2025-07-19 12:01:26 +0530

    Added Methods and Class B from Melvin

---
+++ src/test/java/com/github/Github2.java
@@ -0,0 +1,14 @@
+package com.github;
+
+public class Github2 {
+
+	public void india() {
+		
+		System.out.println("India");
+	}
+	
+	public void england() {
+		
+		System.out.println("England");
+	}
+}
